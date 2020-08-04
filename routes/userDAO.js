75c2ea101d03296ed82ab4c4f2ef84e95e9c2f9c
Deepthamaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2020-08-05 00:18:18 +0530

    Update userDAO.js

--- routes/userDAO.js
+++ routes/userDAO.js
@@ -1,6 +1,4 @@
 
-
-
 const Models = require("../lib/User");
 
 
@@ -31,7 +29,7 @@ async function createUser (x)
     }
 
 async function update(y)
-  { if(await Models(y).save)
+  { if(await Models(y).save())
     {return y
       }
     
@@ -52,53 +50,8 @@ module.exports = {
   update:update
 };
 
-const Models = require("../lib/User");
 
 
 
-async function getUsers (req) {
-  let x=await Models.findOne(req)
-  
-  //console.log(x)
-  if(x){
-    return x
-            //return x
-  }
-  
-}
 
-//const getUsers = async (req) => {
-  //(await Models.find(req)).then((result)=>{ return result}).catch((err)=>{
-    //   console.log(err);
-    //})
-//s}
-
-async function createUser (x)
-{
-  if( await new Models(x).save())
-  {        return x
-  }
-
-    }
-
-async function update(y)
-  { if(await Models(y).save)
-    {return y
-      }
     
-      else{
-        return new error("bad")
-      }
-        
-    
-    }
-
-
-
-
-
-module.exports = {
-  createUser: createUser,
-  getUsers: getUsers,
-  update:update
-};
