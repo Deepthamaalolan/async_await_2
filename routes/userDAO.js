3e43bdf890d8c26da66e5f85313d8e91a38195ba
Deepthamaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2020-08-05 00:13:50 +0530

    Update userDAO.js

--- routes/userDAO.js
+++ routes/userDAO.js
@@ -1,36 +1,101 @@
 
+
+
 const Models = require("../lib/User");
-#updated
-let getUsers = async (req) =>{
-    await (Models.find(req)).then((result)=>{ return  result
-  }).catch((err)=>{
-      console.log(err);
-  })
+
+
+
+async function getUsers (req) {
+  let x=await Models.findOne(req)
+  
+  //console.log(x)
+  if(x){
+    return x
+            //return x
+  }
+  
 }
-function createUser(x)
+
+//const getUsers = async (req) => {
+  //(await Models.find(req)).then((result)=>{ return result}).catch((err)=>{
+    //   console.log(err);
+    //})
+//s}
+
+async function createUser (x)
 {
-  if(new Models(x).save())
+  if( await new Models(x).save())
   {        return x
   }
-  else{
-    return new error("Bad");
-  }
+
     }
 
-     
-function update(y)
-{ if(Models.find(y)){
-  Models(y).save()
-  console.log(y);
-  return y
-  }
-
-  else{
-    return new error("bad")
-  }
+async function update(y)
+  { if(await Models(y).save)
+    {return y
+      }
     
+      else{
+        return new error("bad")
+      }
+        
+    
+    }
 
+
+
+
+
+module.exports = {
+  createUser: createUser,
+  getUsers: getUsers,
+  update:update
+};
+
+const Models = require("../lib/User");
+
+
+
+async function getUsers (req) {
+  let x=await Models.findOne(req)
+  
+  //console.log(x)
+  if(x){
+    return x
+            //return x
+  }
+  
 }
+
+//const getUsers = async (req) => {
+  //(await Models.find(req)).then((result)=>{ return result}).catch((err)=>{
+    //   console.log(err);
+    //})
+//s}
+
+async function createUser (x)
+{
+  if( await new Models(x).save())
+  {        return x
+  }
+
+    }
+
+async function update(y)
+  { if(await Models(y).save)
+    {return y
+      }
+    
+      else{
+        return new error("bad")
+      }
+        
+    
+    }
+
+
+
+
 
 module.exports = {
   createUser: createUser,
